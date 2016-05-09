a346674227ff578bea53e472a42586569cee33e4
qiyu <vp30000@163.com>
2016-05-09 09:06:22 +0800

    other

---
+++ other/jqueryTr.js
@@ -0,0 +1,23 @@
+(function (window,document) {
+    var w = window,
+        doc= document;
+    var Mou = function (selector) {
+        return Mou.prototype.init(selector);
+    }
+    
+    Mou.prototype ={
+        constructor : Mou,
+        length:0,
+        splice:[].splice,
+        selector:'',
+        init:function (selector) {
+            
+        }
+    }
+    Mou.prototype.init.prototype = Mou.prototype;
+    
+    Mou.ajax = function () {
+        console.log(this);
+    }
+    window.f = Mou;
+})(window,document);
