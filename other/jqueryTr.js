5ed80ee65db7b84a93c47b8d7f2f44b8781edf95
miloer <vp30000@163.com>
2016-05-10 11:08:41 +0800

    add tr

--- other/jqueryTr.js
+++ other/jqueryTr.js
@@ -11,9 +11,66 @@
         splice:[].splice,
         selector:'',
         init:function (selector) {
-            
-        }
-    }
+            if(!selector){
+                return this;
+            }  
+            var selector = selector.trim(),
+                elm;
+            if(selector.charAt(0)=='#'&& !selector.match('\\s')){
+                selector = selector.substring(1);
+                this.selector = selector;
+                elm = document.getElementById(selector);
+                this[0]=el;
+                this.length = 1;
+                return this;
+            }else{
+                elm = document.querySelectorAll(selector);
+                for(var i=0; i<elm.length;i++){
+                     this[i] = elm[i];
+                }
+                this.selector = selector;
+                this.length = elm.length;
+                return this;
+            }
+        },
+        css:function (attr,val) {
+            console.log(this.length);
+            for(var i=0;i<this.length;i++){
+                if(arguments.length==1){
+                    return getComputedStyle(this[i],null)[attr];
+                }
+                this[i].style[attr] =val;
+            }
+            return this;
+        },
+      hasClass:function(cls){
+          var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+          for(var i = 0 ;i< this.length ;i++){
+              if(this[i].className.match(reg))
+                return true;
+                return false;
+          }
+          return this;
+      },
+      
+      addClass :function(cls){
+          var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+          for(var i = 0 ;i<this.length;i++){
+              if(!this[i].className.match(reg))
+                  this[i].className+= ''+cls;
+              }
+              return this;
+          },
+       removeClass :function(cls){
+           var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+           for(var i=0 ;i<this.length;i++){
+               if(this[i].className.match(reg))
+                this[i].className = this[i].className.replace(cls,'');
+           }
+           return this;
+       }
+    };
+  
     Mou.prototype.init.prototype = Mou.prototype;
     
     Mou.ajax = function () {
